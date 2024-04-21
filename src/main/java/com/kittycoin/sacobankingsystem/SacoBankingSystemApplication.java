a9de4ba39a66b543a6a3660ab228b897b4c590c9
selenaassou <aselenateaches@gmail.com>
2024-04-21 22:43:41 +0100

    Started new spring boot project!

---
+++ src/main/java/com/kittycoin/sacobankingsystem/SacoBankingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kittycoin.sacobankingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacoBankingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SacoBankingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kittycoin/sacobankingsystem/SacoBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kittycoin.sacobankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacoBankingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
